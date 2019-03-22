f2a1be02e6289afe6d0821d610a87e6b54059b90
ChrisCogs <ccogs@yahoo.com>
2019-03-22 00:57:47 -0400

    Added starting layout and styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,91 @@
-alert("hello");
 
-var newDiv = $("<div>");
+var number = 100;
 
-newDiv.text("Whats up");
+var intervalId;
 
-$("#empty-div").append(newDiv);
+function run(){
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+}
+
+function decrement(){
+    number--;
+    $("#empty-div").html("<h1>" + number + "</h1>");
+}
+
+run();
 
 
 
 
-    // j
 
-    // jQuery alternative to: document.querySelector("#empty-div").appendChild(newDiv);
-    // $("#empty-div").append(newDiv);
 
-    // // If we need to apply some CSS, we can quickly do so, using the jQuery ".attr" method.
-    // newDiv.attr("class", "fancy");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert("hello");
+
+// var newDiv = $("<div>");
+
+// newDiv.text("Whats up");
+
+// $("#empty-div").append(newDiv);
+
+// setTimeout(fiveSeconds, 500 * 5);
+// setTimeout(fourSeconds, 500 * 6);
+// setTimeout(threeSeconds, 500 * 7);
+// setTimeout(twoSeconds, 500 * 8);
+// setTimeout(oneSeconds, 500 * 9);
+// setTimeout(noSeconds, 500 * 10);
+
+
+// function fiveSeconds() {
+//     $("#empty-div").append("<h1>Five Seconds!</h1>");
+//     $("body").css({"background-color": "blue"});
+// }
+// function fourSeconds() {
+//     $("#empty-div").append("<h1>Four Seconds!</h1>");
+//     $("body").css({"background-color": "green"});
+// }
+// function threeSeconds() {
+//     $("#empty-div").append("<h1>Three Seconds!</h1>");
+//     $("body").css({"background-color": "yellow"});
+// }
+// function twoSeconds() {
+//     $("#empty-div").append("<h1>Two Seconds!</h1>");
+//     $("body").css({"background-color": "brown"});
+
+// }
+// function oneSeconds() {
+//     $("#empty-div").append("<h1>One Seconds!</h1>");
+//     $("body").css({"background-color": "orange"});
+
+// }
+// function noSeconds() {
+//     $("#empty-div").append("<h1>Boom!</h1>");
+//     $("body").css({"background-color": "Red"});
+
+// }
+
+
+  
